5132e469ee374354996167013a5ce3d5daff1e71
Viktor Sukhorebrik <viktorsuh1999@gmail.com>
2022-10-15 15:04:22 +0300

    Решение 8 задачи

--- homework_sem1/homework_task_8/Program.cs
+++ homework_sem1/homework_task_8/Program.cs
@@ -3,3 +3,17 @@
 
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+while(count < number)
+{
+    count++;
+    int even_count = count % 2;
+    if(even_count == 0)
+    {
+        Console.WriteLine(count);
+    }
+}
