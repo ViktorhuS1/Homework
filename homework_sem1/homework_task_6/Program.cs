f64e8404b1a455909478be6608cd991d3cd59bf7
Viktor Sukhorebrik <viktorsuh1999@gmail.com>
2022-10-15 14:48:48 +0300

    Решение 6 задачи

--- homework_sem1/homework_task_6/Program.cs
+++ homework_sem1/homework_task_6/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int number_1 = number % 2;
+
+if(number_1 == 0)
+{
+    Console.Write("Число является четным!");
+}
+else
+{
+    Console.Write("Число НЕ является четным!");
+}
