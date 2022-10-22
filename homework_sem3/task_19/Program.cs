aeb2c2cb0059b41d8ebac6a26d9238b250d8c3b5
Viktor Sukhorebrik <viktorsuh1999@gmail.com>
2022-10-22 15:00:04 +0300

    Новое решение задачи с палиндромом

--- homework_sem3/task_19/Program.cs
+++ homework_sem3/task_19/Program.cs
@@ -5,18 +5,39 @@
 // 23432 -> да
 
 
-Console.Write("Введите пятизначное число: ");
-string num = Console.ReadLine();
+// Console.Write("Введите пятизначное число: ");
+// string num = Console.ReadLine();
 
-if(num[0] == num[4] && num[1] == num[3])
+// if(num[0] == num[4] && num[1] == num[3])
+// {
+//     Console.WriteLine("Это число является палиндромом");
+// }
+// else if(num[0] == num[4])
+// {
+//    Console.WriteLine("Это число НЕ палиндром");
+// }
+// else
+// {
+//     Console.Write("Это не пятизначное число!");
+// }
+
+// ВТОРОЕ Решение этой задачи
+
+Console.Write("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int temp = num;
+int rever = 0;
+while(num > 0)
+{
+    int ostatok = num % 10;
+    rever = rever * 10 + ostatok;
+    num = num / 10;
+}
+if(temp == rever)
 {
     Console.WriteLine("Это число является палиндромом");
 }
-else if(num[0] == num[4])
-{
-    Console.WriteLine("Это число НЕ палиндром");
-}
 else
 {
-    Console.Write("Введите пятизначное! число: ");
+    Console.Write("Это не палиндром!");
 }
