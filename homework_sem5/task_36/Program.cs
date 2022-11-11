88f00c64d672d7946a17ce553841c63c5ecd4c1d
Viktor Sukhorebrik <viktorsuh1999@gmail.com>
2022-11-11 14:09:30 +0300

    homework5

---
+++ homework_sem5/task_36/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] array = new int[7];
+
+void FillArray(int[] a)
+{
+    for (int i = 0; i < a.Length; i++)
+    {
+        a[i] = new Random().Next(-50, 50);
+    }
+}
+
+int SumOdd(int[] a)
+{
+    int sum = 0;
+    for (int i = 1; i < a.Length; i += 2)
+    {
+        sum += a[i];
+    }
+    return sum;
+}
+FillArray(array);
+Console.WriteLine(String.Join(", ", array));
+
+int summa = SumOdd(array);
+Console.WriteLine($"Сумма нечетных чисел равна {summa}");
---
+++ homework_sem5/task_38/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+int[] array = new int[5];
+
+void FillArray(int[] a)
+{
+    for (int i = 0; i < a.Length; i++)
+    {
+        a[i] = new Random().Next(1, 100);
+    }
+}
+
+int MinMax(int[] a)
+{
+    int min = a[0];
+    int max = a[0];
+    for (int i = 0; i < a.Length; i++)
+    {
+        if(a[i] < min) min = a[i];
+        if(a[i] > max) max = a[i];
+    }
+    int result = max - min;
+    return result;
+}
+FillArray(array);
+int dif = MinMax(array);
+
+Console.WriteLine(String.Join(", ", array));
+Console.WriteLine($"Разница между мин и макс {dif}");
