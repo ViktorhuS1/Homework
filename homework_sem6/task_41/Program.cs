a51a1e73a30bbad74972d138fb7924fdcdf5c971
Viktor Sukhorebrik <viktorsuh1999@gmail.com>
2022-11-17 17:11:57 +0300

    homework6

---
+++ homework_sem6/task_41/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите числа, после того как ввели, нажмите Enter: ");
+int i = 0;
+int[] array = new int[10];
+
+while(i < array.Length)
+{
+    String input = Console.ReadLine();
+    if(input == "")
+    {
+        break;
+    }
+    else array[i] = int.Parse(input);
+    i++;
+}
+
+
+int CountPositive(int[] a)
+{
+    int count = 0;
+    for (int i = 0; i < a.Length; i++)
+    {
+        if(a[i] > 0) count++;
+    }
+    return count;
+}
+int count = CountPositive(array);
+
+Console.WriteLine($"Чисел больше 0 - {count}");
---
+++ homework_sem6/task_43/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
+//  y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+
+Console.Write("Введите точку k1: ");
+int k1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите точку b1: ");
+int b1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите точку k2: ");
+int k2 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите точку b2: ");
+int b2 = int.Parse(Console.ReadLine());
+
+double k3 = k1 - k2;
+double b3 =  b2 - b1;
+if(k3 < 0) 
+{
+    k3 = k3 * (-1);
+    b3 = b3 * (-1);
+}
+double x = b3 / k3;
+double y = k1 * x + b1;
+
+Console.Write($"Точка пересечения: {x}; {y}");
+
+
+
