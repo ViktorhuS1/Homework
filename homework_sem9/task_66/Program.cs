93492ede672debde4b9421741c9fda5bf85f0292
Viktor Sukhorebrik <viktorsuh1999@gmail.com>
2022-11-27 21:49:28 +0300

    homework9

---
+++ homework_sem9/task_66/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+Console.WriteLine("Введите число 1: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int summa = 0;
+
+int sumNumbers(int firstNum, int lastNum)
+{
+    if (firstNum == lastNum) return summa + lastNum;
+    return summa = summa + (firstNum + sumNumbers(firstNum + 1, lastNum));
+}
+
+Console.WriteLine(sumNumbers(num, num2));
---
+++ homework_sem9/task_68/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+Console.WriteLine("Введите число 1: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int FooAkk(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if ((m > 0) && (n == 0)) return FooAkk(m - 1, 1);
+    if ((m > 0) && (n > 0)) return FooAkk(m - 1, FooAkk(m, n - 1));
+    else return n + 1;
+}
+
+Console.WriteLine(FooAkk(num, num2));
+
